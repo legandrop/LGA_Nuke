a83e0da8b88c06921c68280d86c6efa2dae00687
legandrop <176236735+legandrop@users.noreply.github.com>
2025-02-14 09:20:41 -0300

    flow push debug false

--- Python/Startup/LGA_NKS_Flow/LGA_NKS_Flow_Push.py
+++ Python/Startup/LGA_NKS_Flow/LGA_NKS_Flow_Push.py
@@ -30,7 +30,7 @@ status_translation = {
 }
 
 # Variable global para activar o desactivar los prints // En esta version el Debug se imprime al final del script
-DEBUG = True
+DEBUG = False
 debug_messages = []
 
 
