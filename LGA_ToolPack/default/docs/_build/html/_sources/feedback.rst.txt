06b52aad21eea27b549377786f8e4a4056375186
legandrop <176236735+legandrop@users.noreply.github.com>
2025-04-17 00:18:00 -0300

    LGA_ToolPack + -B - Ya divididos

--- LGA_ToolPack/default/docs/_build/html/_sources/feedback.rst.txt
+++
@@ -1,9 +0,0 @@
-.. _feedback:
-
-Feedback
-========
-I am always happy about feedback. Please write a comment in the comments section at Nukepedia or `drop me a line`_.
-
-Further Nuke plugins can be found at http://www.leafpictures.de and http://www.cragl.com
-
-.. _drop me a line: info@leafpictures.de?subject=nuke-default
--- init.py
+++ init.py
@@ -1,29 +1,32 @@
 import nuke
 
 # Importar menu LGA_ToolPack-Layout
-nuke.pluginAddPath('./LGA_ToolPack-Layout')
+nuke.pluginAddPath("./LGA_ToolPack-Layout")
 
 
 # Importar menu LGA_ToolPack
-nuke.pluginAddPath('./LGA_ToolPack')
+nuke.pluginAddPath("./LGA_ToolPack")
+
+
+# Importar menu LGA_ToolPack
+nuke.pluginAddPath("./LGA_ToolPack-B")
 
 
 # Importar menu LGA_NodePack
-nuke.pluginAddPath('./LGA_NodePack')
+nuke.pluginAddPath("./LGA_NodePack")
 
 
 # Importar LGA_OpenInNukeX
-nuke.pluginAddPath('./LGA_OpenInNukeX')
+nuke.pluginAddPath("./LGA_OpenInNukeX")
 
 
 # Importar LGA_Defaults
-nuke.pluginAddPath('./LGA_Defaults')
+nuke.pluginAddPath("./LGA_Defaults")
 
 
 # Importar las tools de la carpeta tools
-nuke.pluginAddPath('./CollectedTools')
+nuke.pluginAddPath("./CollectedTools")
 
 
 # Importar Cattery
 nuke.pluginAddPath("./Cattery")
-
