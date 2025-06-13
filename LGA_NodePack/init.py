81bf7e95145c03bbd418354313170841bc81710a
legandrop <176236735+legandrop@users.noreply.github.com>
2025-06-13 12:07:48 -0300

    LGA_NodePack v1.32 - init para que funcionen los spin_tools

---
+++ LGA_NodePack/init.py
@@ -0,0 +1,11 @@
+"""
+init especial para spin_tools
+
+"""
+
+import os
+import nuke
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+
+nuke.pluginAddPath(os.path.join(dirname, "spin_tools"))
